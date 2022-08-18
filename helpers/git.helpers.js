9d160f437314145039305326954b3d45b31eed3e
mauro sala <mauro.sala@gmail.com>
2022-08-18 17:37:18 +0200

    feat(helpers): git

---
+++ helpers/git.helpers.js
@@ -0,0 +1,28 @@
+const axios = require('axios')
+const { envConstants } = require('../constants')
+const uriHelpers = require('./uri.helpers')
+const { logger } = require('./logger.helpers')
+const stringHelpers = require('./string.helpers')
+
+const getFile = async ({ endpointName, org, repo, fileName }) => {
+  try {
+    const location = encodeURIComponent(`[${org}][${repo}]${fileName}`)
+
+    const gitUrl = uriHelpers.concatUrl([
+      envConstants.GIT_URI,
+      endpointName,
+      location
+    ])
+    logger.debug(gitUrl)
+
+    const response = (await axios.get(gitUrl)).data
+
+    return stringHelpers.b64toAscii(response.list[0].content)
+  } catch {
+    return null
+  }
+}
+
+module.exports = {
+  getFile
+}
