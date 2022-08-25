23fbf8f09175f14dc9027490cdca627a1e61f345
mauro sala <mauro.sala@gmail.com>
2022-08-25 12:08:50 +0200

    feat(helpers): axios

--- helpers/axios.helpers.js
+++ helpers/axios.helpers.js
@@ -1,27 +1,30 @@
-const errorHandler = (err, res) => {
+const errorHandler = (err) => {
   if (err.response) {
     // The request was made and the server responded with a status code
     // that falls out of the range of 2xx
     // console.log(error.response.data)
     // console.log(error.response.status)
     // console.log(error.response.headers)
-    return res.status(err.response.status).json({
+    return {
+      status: err.response.status,
       message: err.response.data
-    })
+    }
   } else if (err.request) {
     // The request was made but no response was received
     // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
     // http.ClientRequest in node.js
-    return res.status(503).json({
+    return {
+      status: 503,
       message: 'The request was made but no response was received'
-    })
+    }
   }
 
   // Something happened in setting up the request that triggered an Error
-  return res.status(500).json({
+  return {
+    status: 500,
     message:
       'Something happened in setting up the request that triggered an Error'
-  })
+  }
 }
 
 module.exports = {
