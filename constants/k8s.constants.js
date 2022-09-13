cd19e77c18bf536897c12a01ab4270a1a7f3c5d7
mauro sala <mauro.sala@gmail.com>
2022-09-13 12:13:21 +0200

    feat(constants): k8s templates api

--- constants/k8s.constants.js
+++ constants/k8s.constants.js
@@ -1,5 +1,6 @@
 module.exports = {
   strategyApi: '/apis/authn.krateo.io/v1alpha1/strategies',
   templateApi: '/apis/templates.krateo.io/v1alpha1/templates',
-  eventApi: '/api/v1/events'
+  eventApi: '/api/v1/events',
+  deploymentApi: '/apis/apiextensions.k8s.io/v1/customresourcedefinitions'
 }
