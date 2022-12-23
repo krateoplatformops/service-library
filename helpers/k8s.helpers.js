0a767c145120f13b534236c26acefd940a492df1
mauro sala <mauro.sala@gmail.com>
2022-12-23 17:28:58 +0100

    fix(helpers): k8s get resource

--- helpers/k8s.helpers.js
+++ helpers/k8s.helpers.js
@@ -232,6 +232,27 @@ const deleteByName = async (api, name) => {
   })
 }
 
+const getResource = async (api) => {
+  const kc = new k8s.KubeConfig()
+  kc.loadFromDefault()
+
+  const opts = {}
+  kc.applyToRequest(opts)
+
+  return await new Promise((resolve, reject) => {
+    request(
+      encodeURI(`${kc.getCurrentCluster().server}${api}`),
+      opts,
+      (error, response, data) => {
+        if (error) {
+          logger.error(error)
+          reject(error)
+        } else resolve(data)
+      }
+    )
+  })
+}
+
 module.exports = {
   init,
   create,
@@ -241,5 +262,6 @@ module.exports = {
   getList,
   getSingleByName,
   getSingleByUid,
-  deleteByName
+  deleteByName,
+  getResource
 }
